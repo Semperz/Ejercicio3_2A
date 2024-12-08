f4241c07d77873f23490128d2a7c9bdde7b10b37
Sergio <serprigar@gmail.com>
2024-12-09 00:38:44 +0100

    Initial commit

---
+++ src/main/java/edu/badpals/Main.java
@@ -0,0 +1,59 @@
+package edu.badpals;
+
+import edu.badpals.model.Card;
+import edu.badpals.model.Course;
+import edu.badpals.model.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            Card card1 = new Card("_123", "fp", null);
+            Card card2 = new Card("_456", "eso", null);
+
+            Course course = new Course("Ciencias", null, null);
+
+            Student student1 = new Student("Francisco Garrido", "23456789A", "123456789", card1);
+            Student student2 = new Student("Ana Fernandez", "00000000T", "897878787", card2);
+
+            student1.getCourses().add(course);
+            student2.getCourses().add(course);
+            course.getStudents().add(student1);
+            course.getStudents().add(student2);
+
+            em.persist(card1);
+            em.persist(card2);
+            em.persist(course);
+            em.persist(student1);
+            em.persist(student2);
+
+            em.getTransaction().commit();
+
+            System.out.println("Estudiantes creados:");
+            System.out.println(student1);
+            System.out.println(student2);
+
+/*            em.getTransaction().begin();
+            em.remove(student1);
+            em.getTransaction().commit();
+
+            System.out.println("Estudiante 1 eliminado");
+            System.out.println("Su carta: " + card1);*/
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
